d6fe9de3394fed0b756e66e1d945802364f4cb9f
netomn010-cloud <netomn010@gmail.com>
2026-09-10 20:53:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alet("0 javaScript esta fucionando!");
+let segundos = 0;
+let intervalo = null;
+
+function iniciar() {
+intervalo = setInterval(function() {
+segundos++;
+
+let horas = Math.floor(segundos / 3600);
+let minutos = Math.floor((segundos % 3600) / 60);
+let segundosRestantes = segundos % 60;
+
+horas = String(horas).padStart(2, "0");
+minutos = String(minutos).padStart(2, "0");
+segundosRestantes = String(segundosRestantes).padStart(2, "0");
+
+document.getElementById("display").innerText =
+horas + ":" + minutos + ":" + segundosRestantes;
+}, 1000);
+}
+
+function pausar() {
+clearInterval(intervalo);
+intervalo = null;
+}
+
+function zera() {
+clearInterval(intervalo);
+intervalo = null;
+segundos = 0;
+
+document.getElementById("display").innerText = "00:00:00";
+}
