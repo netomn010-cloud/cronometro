2e6166194c11abe3582d43667e13b781d0b52b37
netomn010-cloud <netomn010@gmail.com>
2026-09-10 20:27:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ let segundos = 0;
 let intervalo = 0;
 
 function iniciar() {
+    intervalo = setInterval(functon() {
     segundos++;
     document.getElementByld("display").innerText = segundos;
   }, 1000);
 }
+
 function pausar() {
   clearlnterval(intervalo);
 }
+
 function zera() {
   clearlnterval(intervalo);
   segundos = 0;
